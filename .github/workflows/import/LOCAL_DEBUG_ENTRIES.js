917de9a2a1efdb4a718bf4e77817a86ebe0b3465
Okan Sahin <osahin@adobe.com>
2025-05-27 15:01:15 +0200

    wp

---
+++ .github/workflows/import/LOCAL_DEBUG_ENTRIES.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    "path": "/africa/fragments/products/cards/headless-cms",
+    "route": "preview"
+  },
+  {
+    "path": "/africa/fragments/products/cards/journey-optimizer-b2b-edition",
+    "route": "preview"
+  },
+  {
+    "path": "/africa/fragments/products/cards/learning-manager",
+    "route": "preview"
+  }
+]
